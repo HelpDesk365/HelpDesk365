be9660ce6ffd37b3890d1d4b963c6418826f3287
mjg7822 <m78mjg@hotmail.com>
2015-01-29 17:45:34 +0900

    server logic change

---
+++ WinFormsServer/Hubs/Agent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    class Agent
+    {
+        public string AgentId { get; set; }
+        public string AgentName { get; set; }
+        public List<Group> Groups { get; set; }
+    }
+
+    class Group 
+    {
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+        public List<Client> Clients { get; set; }
+        public bool IsConnected { get; set; }
+    }
+
+    class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+    }
+}
---
+++ WinFormsServer/Hubs/ChatHub.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    class ChatHub : Hub
+    {
+        public static List<Agent> agentList = new List<Agent>();
+        public void Send(string name, string agentId, string message, string groupId)
+        {
+            Clients.Group(groupId).addMessage(name, message);
+        }
+        public override Task OnConnected()
+        {
+            Program.MainForm.WriteToConsole("Client connected: " + Context.ConnectionId);
+            return base.OnConnected();
+        }
+       
+        public override Task OnDisconnected()
+        {
+            var agent = agentList.FirstOrDefault(d => d.Groups.Count(o => o.GroupId == Context.ConnectionId) > 0);
+            if (agent != null)
+            {
+                var group  = agent.Groups.FirstOrDefault(d=>d.GroupId == Context.ConnectionId);                
+                foreach (var item in group.Clients)
+                {
+                    Groups.Remove(item.ClientId, group.GroupId);
+                }
+                agent.Groups.Remove(group);
+            }
+
+            Program.MainForm.WriteToConsole("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnected();
+        }        
+
+       
+
+    }
+}
